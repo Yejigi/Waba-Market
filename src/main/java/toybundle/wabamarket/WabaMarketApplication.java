f4a89a7d13241c510518a758c9919b7e2f9dff5d
Yejigi <jyj000207@naver.com>
2022-03-21 00:36:23 +0900

    add/jyj : 파일추가

---
+++ src/main/java/toybundle/wabamarket/WabaMarketApplication.java
@@ -0,0 +1,13 @@
+package toybundle.wabamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WabaMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WabaMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toybundle/wabamarket/WabaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package toybundle.wabamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WabaMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
